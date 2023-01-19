b99148a2a5a7b43d0916700cdb5fa594393b7a5c
farazchohan3 <frazchohan007@gmail.com>
2023-01-19 15:28:00 +0500

    Intial Changing

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,70 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Course } from '../models/course';
+import { Curriculum } from '../models/curriculum';
+import { Term } from '../models/term';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  userModel: User;
+  httpHeaders: HttpHeaders = new HttpHeaders({
+    'Cache-Control': 'no-cache'
+  });
+
+  curriculumsSub = new BehaviorSubject<Curriculum[]>([]);
+  curriculumRetrieved: boolean = false;
+
+  coursessSub = new BehaviorSubject<Course[]>([]);
+  coursesRetrieved: boolean = false;
+
+  termsSub = new BehaviorSubject<Term[]>([]);
+  termsRetrieved: boolean = false;
+
+  constructor(
+    private httpClient: HttpClient
+  ) { 
+    this.userModel = JSON.parse(localStorage.getItem('user') || "");
+  }
+
+  getCurriculums(): void {
+    if(!this.curriculumRetrieved) {
+      this.httpClient.get<{ curriculums: any[] }>(`${environment.serverUrl}/curriculums`, {
+        headers: this.httpHeaders
+      })
+      .toPromise()
+      .then(res => {
+        this.curriculumsSub.next(res?.curriculums.filter(e => e.deptName === this.userModel?.department).map(e => e as Curriculum));
+        this.curriculumRetrieved = true;
+      }, err => {
+        console.log(">>> error: ", err);
+        
+      });
+    }
+  }
+
+  getCourses(): void {
+    if(!this.coursesRetrieved) {
+      this.httpClient.get<{ courses: Course[] }>(`${environment.serverUrl}/courses`, {
+        headers: this.httpHeaders
+      })
+      .toPromise()
+      .then((res) => {
+        this.coursessSub.next(res?.courses.filter(x => x.courseOwnerId === this.userModel._id));    
+        this.coursesRetrieved = true;
+      }, (err) => {
+        console.log(">>> error: ", err);
+        
+      })
+    }
+  } 
+}
+
+/**
+ * batchId, termId => courses fetch OBE Server Route
+ */
