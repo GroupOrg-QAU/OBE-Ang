7a3d3fc5848d0570bad427902b3b67effd4c26a3
fazal-a <thefazalabbas@gmail.com>
2023-02-07 23:28:33 +0500

    added terms section again in working  condition

---
+++ src/app/sub-layouts/batch/batch-routing.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {BatchesComponent} from 'src/app/pages/batches/batches.component';
+import {CoMappingComponent} from 'src/app/pages/co-mapping/co-mapping.component';
+import {CourseOutcomeComponent} from 'src/app/pages/course-outcome/course-outcome.component';
+import {CoursesComponent} from 'src/app/pages/courses/courses.component';
+import {AddTermComponent} from "../../pages/add-term/add-term.component";
+
+const routes: Routes = [
+  {path: '', redirectTo: 'courses', pathMatch: 'full'},
+  {path: 'courses/:courseId/course-outcomes', component: CourseOutcomeComponent},
+  {path: 'batches', component: BatchesComponent},
+  {path: 'add-term', component: AddTermComponent},
+  {path: 'courses', component: CoursesComponent},
+  {path: 'co-mapping', component: CoMappingComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class BatchRoutingModule {
+}
